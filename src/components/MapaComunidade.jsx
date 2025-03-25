2342374eacd8ea59b6ea640e7d8b1a48d6ab65c0
João M. <jvmarchette02003@gmail.com>
2025-03-25 00:51:47 -0300

    Feat: Configuração do docker

--- src/components/MapaComunidade.jsx
+++ src/components/MapaComunidade.jsx
@@ -11,7 +11,7 @@ const MapaComunidade = () => {
         center={centroBairro}
         zoom={15}
       >
-        {/* Mapa sem coordenadas */}
+        {}
       </GoogleMap>
     </LoadScript>
   );
